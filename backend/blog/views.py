5e567d1f89bcd4ceb47b13713e35acd6331a233a
kila <luka.kilasonia.1@btu.edu.ge>
2024-11-27 20:47:19 +0400

    view and url for all blog

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+
+from .models import Blog
+from .serializers import BlogSerializer
 
 # Create your views here.
+
+class BlogsListApi(ListAPIView):
+      queryset = Blog.objects.all()
+      serializer_class = BlogSerializer
+      permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+
+    #   return Response(serializer.data, status=status.HTTP_200_OK) 
