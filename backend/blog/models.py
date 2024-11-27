821096632c890948b5ca21af40278212bd3526a3
kila <luka.kilasonia.1@btu.edu.ge>
2024-11-27 20:45:57 +0400

    create user and blog init model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class User(models.Model):
+    name = models.TextField(max_length=30, null=False)
+    lastname = models.TextField(max_length=100, null=False)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_created=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs')
+
+    def __str__(self) -> str:
+        return self.title
+
