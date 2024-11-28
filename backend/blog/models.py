069999690456533fcac7ed9d5e5d52c80d003396
kila <luka.kilasonia.1@btu.edu.ge>
2024-11-28 16:40:01 +0400

    [fix] datetime autofield

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -17,7 +17,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     image = models.ImageField()
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs')
 
     def __str__(self) -> str:
