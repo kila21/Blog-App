bd4a8f497090e15017686cc4f3473aa9c0408d42
kila <luka.kilasonia.1@btu.edu.ge>
2024-12-04 07:19:48 +0400

    remove custom user model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,17 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-
-class User(models.Model):
-    name = models.TextField(max_length=30, null=False)
-    lastname = models.TextField(max_length=100, null=False)
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.name
-    
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
